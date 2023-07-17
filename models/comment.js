1c86b251a9318bb799c67d4817ee4eb49a49266d
ericz02 <ezheng0720@gmail.com>
2023-07-16 21:46:27 -0400

    migrates comment model

---
+++ models/comment.js
@@ -0,0 +1,50 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      this.belongsTo(models.Post, { onDelete: "CASCADE" });
+      this.belongsTo(models.User);
+    }
+  }
+  Comment.init(
+    {
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+        validation: {
+          len: {
+            args: [1, 255],
+            msg: "Comment must be between 1 and 255 characters",
+          },
+        },
+      },
+      UserId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "Users",
+          key: "id",
+        },
+      },
+      PostId: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: "posts",
+          key: "id",
+        },
+      },
+    },
+    {
+      sequelize,
+      modelName: "Comment",
+      tableName: "comments",
+    }
+  );
+  return Comment;
+};
